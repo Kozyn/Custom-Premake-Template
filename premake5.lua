45245c4b4dcc153600cd541797a97f595b6f0474
Kozyn <kozynthetaquito@gmail.com>
2025-03-13 23:41:31 -0400

    Added Fields For Library Directories and Files

--- premake5.lua
+++ premake5.lua
@@ -18,12 +18,43 @@ workspace "MyApp"
     -- Set project files location
     location "Project"
 
+    -- Library Directories --
+    libdirs {
 
+    }
+
+    -- Debug Configuration --
     filter "configurations:Debug"
         symbols "On"
 
+        -- Debug Specific Library Directories --
+        libdirs {
+
+        }
+
+        -- Debug Specific Libraries --
+        links {
+
+        }
+
+    -- Release Configuration --
     filter "configurations:Release"
         optimize "On"
+
+        -- Release Specific Library Directories --
+        libdirs {
+
+        }
+
+        -- Release Specific Libraries --
+        links {
+            
+        }
+
+    -- Global Libraries --
+    links {
+    
+    }
 
     -- Include subprojects
     include "Core"
